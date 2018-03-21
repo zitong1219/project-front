77062423ddd2a5f3adc3cdced3213999b751b95f
ZSY <1169846461@qq.com>
2018-03-21 15:22:19 +0800

    zsy master create store/modules/permission

--- src/store/modules/permission.js
+++ src/store/modules/permission.js
@@ -1,1 +1,59 @@
 import { asyncRouterMap, constantRouterMap } from '@/router'
+
+const permission = {
+  state: {
+    routers: constantRouterMap,
+    addRouters: []
+},
+
+  mutation: {
+    SET_ROUTERS: (state, routers) => {
+      state.addRouters = routers
+      state.routers = constantRouterMap.concat(routers)
+    }
+  },
+
+  action: {
+    generateRoutes({ commit }, data) {
+      return new Promise(resolve => {
+        const { roles } = data
+        let accessedRouters
+        /* roles中有admin则加载全部asyncRouterMap 
+         * 否则调用filterAsyncRouter一个个解析roles里的元素
+         */
+        if (roles.indexOf('admin' >= 0)) {
+          accessedRouters = asyncRouterMap
+        }
+        else {
+          accessedRouters = filterAsyncRouter(asyncRouterMap, roles)
+        }
+        commit('SET_ROUTERS', accessedRouters)
+        resolve()
+      })
+    }
+  }
+}
+
+function filterAsyncRouter(asyncRouterMap, roles) {
+  const accessedRouters = asyncRouterMap.filter(route => {
+    if (accessPermission(roles, route)) {
+      if (route.children && route.children.length) {
+        route.children = filterAsyncRouter(route.children, roles)
+      }
+      return true
+    }
+    else {
+      return false
+    }
+  })
+  return accessedRouters
+}
+
+function accessPermission(roles, route) {
+  if (route.meta && route.meta.roles) {
+    return roles.some(role => route.meta.roles.indexOf(role) >= 0)
+  }
+  else {
+    return true
+  }
+}
