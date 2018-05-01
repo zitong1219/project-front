5086b948b175c9b7f34208dcb055e857e9260327
ZSY <1169846461@qq.com>
2018-05-01 15:36:43 +0800

    zsy master delete useless styles

--- src/views/AnalysisAndJudgment/deviceShapeAnalysis.vue
+++ src/views/AnalysisAndJudgment/deviceShapeAnalysis.vue
@@ -32,13 +32,5 @@ export default {
 </script>
 
 <style rel="stylesheet/scss" lang="scss" scoped>
-.dashboard {
-  &-container {
-    margin: 30px;
-  }
-  &-text {
-    font-size: 30px;
-    line-height: 46px;
-  }
-}
+
 </style>
